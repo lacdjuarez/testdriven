642324763ae7eb9e167ee503f1f1fb90c3f97440
lacdjuarez <lacdjuarez@gmailc.om>
2016-04-12 21:39:47 -0600

    evaluate replace the placeholder with the value

--- src/Template.java
+++ src/Template.java
@@ -4,12 +4,14 @@
 
 public class Template {
     private String variableValue;
+    private String templateText;
     public Template(String templateText) {
+        this.templateText = templateText;
     }
     public void set(String variable, String value) {
         this.variableValue = value;
     }
     public String evaluate() {
-        return "Hello, " + variableValue;
+        return templateText.replaceAll("\\$\\{name\\}", variableValue);
     }
 }
